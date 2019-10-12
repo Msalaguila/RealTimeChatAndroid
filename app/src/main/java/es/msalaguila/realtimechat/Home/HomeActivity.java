e307e56a0d9a6719a1bef23f2087759b8cc409cb
Manuel <msalaguila@gmail.com>
2019-10-12 16:02:29 +0100

    Creating Home Toolbar

--- app/src/main/java/es/msalaguila/realtimechat/Home/HomeActivity.java
+++ app/src/main/java/es/msalaguila/realtimechat/Home/HomeActivity.java
@@ -44,7 +44,7 @@ public class HomeActivity
     //Log.e(TAG, "displayData()");
 
     // deal with the data
-    ((TextView) findViewById(R.id.data)).setText(viewModel.data);
+    // ((TextView) findViewById(R.id.data)).setText(viewModel.data);
   }
 
   @Override
